ece90de353422f79e793eaf2912d44b4a73afd8c
charleschan1998 <charleschan2016@m.scnu.edu.cn>
2022-12-27 20:35:41 +0800

    FEAT tailwind引入

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Hello, world</div>
+  <div class="flex justify-center items-center w-screen h-screen">
+    <div class="text-xl text-red-500">Hello, world</div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
